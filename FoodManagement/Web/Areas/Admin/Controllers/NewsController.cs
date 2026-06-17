32e101e05d532550f4d156f9007a55d438557c95
Trang Uyên <uyentrang8058@outlook.com>
2026-06-17 20:45:36 +0700

    Lab 12

---
+++ FoodManagement/Web/Areas/Admin/Controllers/NewsController.cs
@@ -0,0 +1,137 @@
+﻿using Core.Database.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Dynamic.Core;
+using Web.Areas.Admin.Extensions;
+using Web.Areas.Admin.Models;
+using Web.Models.EF;
+
+namespace Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class NewsController : Controller
+    {
+        private readonly FoodContext _dbContext;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment;
+        public NewsController(FoodContext dbContext, Microsoft.AspNetCore.Hosting.IHostingEnvironment environment)
+        {
+            _dbContext = dbContext;
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public async Task<IActionResult> getList(jDatatable model)
+        {
+            var items = (from i in _dbContext.Articles select i);
+            int recordsTotal = 0;
+            if (!string.IsNullOrEmpty(model.columns[model.order[0].column].name) && !string.IsNullOrEmpty(model.order[0].dir))
+            {
+                items = items.OrderBy(model.columns[model.order[0].column].name + ' ' + model.order[0].dir);
+            }
+            if (!string.IsNullOrEmpty(model.search.value))
+            {
+                items = items.Where(i => i.Title.Contains(model.search.value));
+            }
+            recordsTotal = items.Count();
+            var data = await items.Select(i => new
+            {
+                i.Id,
+                i.Title,
+                i.CreatedOn
+            }).Skip(model.start).Take(model.length).ToListAsync();
+            var jsonData = new { draw = model.draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data };
+            return Ok(jsonData);
+        }
+        [HttpGet]
+        public async Task<IActionResult> getItem(Guid id)
+        {
+            if (_dbContext.Articles == null)
+                return NotFound();
+            var item = await _dbContext.Articles.FindAsync(id);
+            if (item == null)
+                return NotFound();
+            return Ok(item);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Save(NewsViewModel model, IFormFile Picture)
+        {
+            Core.Database.Models.Article item;
+            var loggedMember = HttpContext.Session.GetObject<Member>("member");
+            if (model.Id == null)
+            {
+                item = new Core.Database.Models.Article();
+                item.Id = Guid.NewGuid();
+                item.CreatedBy = loggedMember.Id;
+                item.CreatedOn = DateTime.Now;
+                await _dbContext.Articles.AddAsync(item);
+
+            }
+            else
+            {
+                item = await _dbContext.Articles.FindAsync(model.Id);
+                item.CreatedBy = loggedMember.Id;
+                item.ModifiedOn = DateTime.Now;
+            }
+            item.Title = model.Title;
+            item.Content = model.Content;
+            item.Keyword = model.Keyword;
+            item.Description = model.Description;
+
+            if (Picture != null)
+            {
+                var path = Path.Combine(this._environment.WebRootPath, "img/news/", Picture.FileName);
+                using (FileStream stream = new FileStream(path, FileMode.Create))
+                {
+                    await Picture.CopyToAsync(stream);
+                    stream.Close();
+                }
+                item.Picture = "/img/news/" + Picture.FileName;
+            }
+            await _dbContext.SaveChangesAsync();
+            return Ok(item);
+
+        }
+        [HttpGet]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+
+                var item = await _dbContext.Articles.FindAsync(id);
+                string path = this._environment.WebRootPath + item.Picture;
+                _dbContext.Entry(item).State = EntityState.Deleted;
+                await _dbContext.SaveChangesAsync();
+                if (System.IO.File.Exists(path))
+                {
+                    System.IO.File.Delete(path);
+                }
+                return Ok(true);
+
+            }
+            catch
+            {
+                return Ok(false);
+            }
+        }
+
+            [HttpPost]
+            public async Task<IActionResult> UploadPicture(IFormFile Picture)
+            {
+                if (Picture != null)
+                {
+                    var path = Path.Combine(this._environment.WebRootPath, "img/news/", Picture.FileName);
+                    using (FileStream stream = new FileStream(path, FileMode.Create))
+                    {
+                        await Picture.CopyToAsync(stream);
+                    }
+                    return Ok("/img/news/" + Picture.FileName);
+                }
+
+                return Ok("");
+            }
+        
+    }
+}
---
+++ FoodManagement/Web/Migrations/20260617123007_s5.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class s5 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authorized",
+                keyColumn: "Id",
+                keyValue: new Guid("a2fda332-14ae-404f-839b-df07b181b388"));
+
+            migrationBuilder.DropColumn(
+                name: "Intro",
+                table: "Article");
+
+            migrationBuilder.InsertData(
+                table: "Authorized",
+                columns: new[] { "Id", "GroupId", "RoleId" },
+                values: new object[] { new Guid("95170f39-ea06-493b-85bf-61183e7d8bee"), new Guid("d0cfdf00-afc9-4567-a9ec-0f0db44a18bd"), new Guid("b5f8852e-1a0f-4fee-a821-1c982c33f9aa") });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("5ea90800-ec76-4e3c-83f5-0d7446510385"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7325));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("6b04bd80-c414-4836-ac8c-ca215b574f41"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7338));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("74d373c3-dcf6-4634-8a46-7700b82dbe4d"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7332));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("86700b84-de54-426a-9748-da1bce88e424"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7335));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("d951dd74-a153-408c-ab60-44e51bb51f47"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7328));
+
+            migrationBuilder.UpdateData(
+                table: "Member",
+                keyColumn: "Id",
+                keyValue: new Guid("fd48367d-f4a1-4e0b-a1f6-9d72afcebcc9"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 19, 30, 3, 469, DateTimeKind.Local).AddTicks(7235));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Authorized",
+                keyColumn: "Id",
+                keyValue: new Guid("95170f39-ea06-493b-85bf-61183e7d8bee"));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Intro",
+                table: "Article",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Authorized",
+                columns: new[] { "Id", "GroupId", "RoleId" },
+                values: new object[] { new Guid("a2fda332-14ae-404f-839b-df07b181b388"), new Guid("d0cfdf00-afc9-4567-a9ec-0f0db44a18bd"), new Guid("b5f8852e-1a0f-4fee-a821-1c982c33f9aa") });
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("5ea90800-ec76-4e3c-83f5-0d7446510385"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3094));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("6b04bd80-c414-4836-ac8c-ca215b574f41"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3119));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("74d373c3-dcf6-4634-8a46-7700b82dbe4d"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3107));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("86700b84-de54-426a-9748-da1bce88e424"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3113));
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: new Guid("d951dd74-a153-408c-ab60-44e51bb51f47"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3100));
+
+            migrationBuilder.UpdateData(
+                table: "Member",
+                keyColumn: "Id",
+                keyValue: new Guid("fd48367d-f4a1-4e0b-a1f6-9d72afcebcc9"),
+                column: "CreatedOn",
+                value: new DateTime(2026, 6, 17, 16, 34, 24, 50, DateTimeKind.Local).AddTicks(3032));
+        }
+    }
+}
